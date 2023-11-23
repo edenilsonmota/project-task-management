3b37e9735601965150bbcb6db03293041c90c84e
Edenilson Mota <edenilsonmota.job@gmail.com>
2023-11-22 22:05:58 -0300

    Add: configurações globais

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+//DB credentials
+define("HOST", 'localhost');
+define("DB", 'crud');
+define("USER", 'esm');
+define("PASS", 'teste123');
+
+define("DS", DIRECTORY_SEPARATOR); //separador
+define("DIR_APP", __DIR__); //raiz
+define("DIR_PROJECT", 'crud-tarefas');  //pasta do projeto
+
+
+//carregar autoload
+if(file_exists('autoload.php')){
+    require 'autoloadd.php';
+}else{
+    throw new Exception('Erro: O arquivo "autoload.php" não foi encontrado.');
+}
+
+?>
