ef439b002de009ab476b945a64e72a9d15cdcfd4
Edenilson Mota <edenilsonmota.job@gmail.com>
2023-11-23 02:35:56 -0300

    Refactor: melhora na estrutura do Model

--- Model/DB/Conn.php
+++ Model/DB/Conn.php
@@ -4,7 +4,6 @@ namespace DB;
 
 use PDO;
 use PDOException;
-use InvalidArgumentException;
 
 class Conn
 {
@@ -33,101 +32,70 @@ class Conn
             throw new PDOException($exception->getMessage());
         }
     }
+
     /**
      * Listar(SELECT) todos os registros
-     *
      * @return array
      */
     public function getAll() : array
     {
-        try {
-            $query = $this->conexao->query("SELECT * FROM task");
-            return $query->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $exception) {
-            throw new PDOException($exception->getMessage());
-        }
+        $query = $this->conexao->query("SELECT * FROM task");
+        return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 
     /**
      * Listar(SELECT) tum registro por id
-     *
      * @param integer $id
-     * 
+     * @return array
      */
-    public function getOneByKey(int $id)
+    public function getOneByKey(int $id) : array
     {
-        try {
-            $stmt = $this->conexao->prepare("SELECT * FROM task WHERE id = :id");
-            $stmt->bindParam(':id', $id);
-            $stmt->execute();
-    
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $exception) {
-            echo "Erro ao executar a consulta: " . $exception->getMessage();
-        }
+        $stmt = $this->conexao->prepare("SELECT * FROM task WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     /**
      * Inserir SQL task
-     *
      * @param string $titulo
      * @param string $descricao
-     * 
+     * @return bool
      */
     public function insert(string $titulo, string $descricao)
     {
-        try {
-            $stmt = $this->conexao->prepare("INSERT INTO task (titulo, descricao) VALUES (:titulo, :descricao)");
-            $stmt->bindParam(':titulo', $titulo);
-            $stmt->bindParam(':descricao', $descricao);
-            $stmt->execute();
-
-            echo "Inserção bem-sucedida!";
-        } catch (PDOException $exception) {
-            echo "Erro ao executar a inserção: " . $exception->getMessage();
-        }
+        $stmt = $this->conexao->prepare("INSERT INTO task (titulo, descricao) VALUES (:titulo, :descricao)");
+        $stmt->bindParam(':titulo', $titulo);
+        $stmt->bindParam(':descricao', $descricao);
+        return $stmt->execute();
     }
 
     /**
      * Update SQL task
-     *
      * @param integer $id
      * @param string $titulo
      * @param string $descricao
-     * 
+     * @return bool
      */
     public function update(int $id, string $titulo, string $descricao)
     {
-        try {
-            $stmt = $this->conexao->prepare("UPDATE task SET titulo = :titulo, descricao = :descricao WHERE id = :id");
-            $stmt->bindParam(':titulo', $titulo);
-            $stmt->bindParam(':descricao', $descricao);
-            $stmt->bindParam(':id', $id);
-            $stmt->execute();
-
-            echo "Atualização bem-sucedida!";
-        } catch (PDOException $exception) {
-            echo "Erro ao executar a atualização: " . $exception->getMessage();
-        }
+        $stmt = $this->conexao->prepare("UPDATE task SET titulo = :titulo, descricao = :descricao WHERE id = :id");
+        $stmt->bindParam(':titulo', $titulo);
+        $stmt->bindParam(':descricao', $descricao);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
     }
 
     /**
      * Update SQL task
-     *
      * @param int $id
-     * 
+     * @return bool
      */
     public function delete(int $id)
     {
-        try {
-            $stmt = $this->conexao->prepare("DELETE FROM task WHERE id = :id");
-            $stmt->bindParam(':id', $id);
-            $stmt->execute();
-
-            echo "Exclusão bem-sucedida!";
-        } catch (PDOException $exception) {
-            echo "Erro ao executar a exclusão: " . $exception->getMessage();
-        }
+        $stmt = $this->conexao->prepare("DELETE FROM task WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
     }
 
 
