5218b550544af6b154274bd9dbf826eeabb992cd
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-08-06 02:11:14 -0300

    add composer

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 
-$host = "mysql";
-$db = "task";
-$user = "admin";
-$pass = "admin";
-
-try {
-
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-
-    if($pdo) {
-        echo "Connected to the $db database successfully!";
-    }
-} catch( PDOException $e){
-    die($e->getMessage());
-} finally {
-    if($pdo){
-        $pdo = null;
-    }
-}
+require "vendor/autoload.php";
