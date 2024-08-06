9a078770f41bdcc10377defd1a68c62bfa7cb4a9
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-08-06 01:41:50 -0300

    Dockerfile

--- index.php
+++ index.php
@@ -1,36 +1,21 @@
 <?php
-/* ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); */
 
-//carregar constantes
-require __DIR__ . '/App/Config/config.php';
+$host = "mysql";
+$db = "task";
+$user = "admin";
+$pass = "admin";
 
-// Iniciar sessão se não estiver ativa
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    if($pdo) {
+        echo "Connected to the $db database successfully!";
+    }
+} catch( PDOException $e){
+    die($e->getMessage());
+} finally {
+    if($pdo){
+        $pdo = null;
+    }
 }
-// Função para gerar um token CSRF
-function generateCsrfToken() {
-    return bin2hex(random_bytes(32));
-  }
-  
-  // Verificar se o token CSRF já existe na sessão, senão gerar um novo
-  if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = generateCsrfToken();
-  }
-
-/* echo "Index:";
-var_dump($_SESSION);die; */
-
-
-
-if(empty($_SESSION['user']) && empty($_SESSION['pass']) && $_SESSION['status'] !== 'LOGADO'){
-    // Limpar variáveis da sessão antes de redirecionar para a página de login
-    session_unset();
-    session_destroy();
-    header("Location: " . URL . "App/View/login.php");
-}else{
-    header("Location: " . URL . "App/View/home.php");
-}
-
