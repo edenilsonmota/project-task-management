34c3e613383d6c44b7520794334b92d4bc01b5e4
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-08-20 02:56:45 -0300

    ajust index for routes

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 require "vendor/autoload.php";
+
+use app\router\Router;
+use app\view\TaskView;
+
+
+
+$uri = parse_url($_SERVER['REQUEST_URI']);
+
+
+if($uri['path'] !== "/"){
+    //chamar rota
+   Router::execute();
+}else{
+    //TaskView::index();
+}
